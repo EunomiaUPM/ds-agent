cdc483b73d46feaf1ae75d8adb388c52571e5174
Rodrigo Menéndez <rodmenm@gmail.com>
2026-04-14 17:02:27 +0200

    Minor correction on dev

--- deployment/dev/run-consumer-dev.sh
+++ deployment/dev/run-consumer-dev.sh
@@ -12,7 +12,7 @@ export RUST_BACKTRACE="1"
 
 # 2. Levantar dependencias
 echo -e "\033[0;36mLevantando dependencias (consumer)...\033[0m"
-docker-compose -f docker-compose.mini.dev.consumer.yaml up -d
+docker compose -f docker-compose.mini.dev.consumer.yaml up -d
 
 # 3. Esperar a que la DB esté lista
 echo -e "\033[0;36mEsperando a que la DB esté lista...\033[0m"
@@ -21,23 +21,28 @@ until docker exec consumer-db pg_isready -U postgres > /dev/null 2>&1; do
 done
 echo -e "\033[0;32mDB lista\033[0m"
 
-# 4. Setup
-echo -e "\033[0;36mCompilando React...\033[0m"
-cd ../../crates/bff
-cargo run build -e ../../static/environment/config/dev/dev.consumer.yaml
-if [ $? -ne 0 ]; then
-    echo -e "\033[0;31mCompilado de react fallido, abortando\033[0m"
-    exit 1
-fi
+# 4. 🔥 BUILD FRONTEND (FUERA DEL BFF)
+echo -e "\033[0;36mConstruyendo frontend React (externo)...\033[0m"
 
+cd ../../gui
+
+npm install
+npm run build -w admin
+
+cd ../crates/bff
+
+echo -e "\033[0;32mFrontend compilado correctamente\033[0m"
+
+# 5. Setup backend
 echo -e "\033[0;36mEjecutando setup...\033[0m"
 cd ../monolith
+
 cargo run setup -e ../../static/environment/config/dev/dev.consumer.yaml
 if [ $? -ne 0 ]; then
     echo -e "\033[0;31mSetup fallido, abortando\033[0m"
     exit 1
 fi
 
-# 5. Start
+# 6. Start
 echo -e "\033[0;36mArrancando consumer...\033[0m"
 cargo watch -x "run start -e ../../static/environment/config/dev/dev.consumer.yaml"
